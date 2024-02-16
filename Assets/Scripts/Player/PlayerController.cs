4292724c8627e7d9172554cb36feadce75a34a56
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2024-02-16 18:14:16 +0200

    aloitettiin player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D body;
+
+    private Master controls;
+
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        controls = new Master();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
     }
 }
